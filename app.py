2aa6605ef59faf1fe50d51ba435e5edfdece4854
emsawant <119991549+emsawant@users.noreply.github.com>
2024-01-13 15:30:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, yeh ek flask application hai.'
+    return 'Hello world from DMCE.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 5000)
